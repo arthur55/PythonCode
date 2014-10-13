5067331a18c323e1a910fe89d1eb679c6269860c
arthur55 <art841213@gmail.com>
2014-10-13 14:40:46 +0800

    new
    new

---
+++ if.py
@@ -0,0 +1,13 @@
+x = input("Where are you from?\nTaiwan, Chinese, Japanese, Korean or HongKong?\n")
+if x == "Taiwan" :
+    print ("God!")
+elif x == "Chinese" :
+    print ("Fuck you!")
+elif x== "Japanese" :
+	print ("Come on!")
+elif x== "Korean" :
+	print ("Dog!")
+elif x == "HongKong" :
+	print ("Cheer up!")
+else :
+	print ("Are you ET?")
